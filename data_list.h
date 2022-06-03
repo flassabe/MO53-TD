a3d7b5125b7c36cda43906858c0c91f2b2b09e02
antoine <antoine.richard68@gmail.com>
2022-06-03 05:06:55 +0200

    add doxygen comments class list_element

--- data_list.h
+++ data_list.h
@@ -1,3 +1,17 @@
+/**
+ * 
+ * @file data_list.h
+ * @author antoine richard
+ * @date 2022-06-03
+ * @brief Contains all classes used to represent a chained list of doubles :
+ * - list_element
+ * - value_list
+ * - data_list
+ * 
+ */
+
+//
+// Define guards
 #ifndef DATA_LIST_H
 #define DATA_LIST_H
 
@@ -5,21 +19,91 @@
 #include <vector>
 #include <string>
 
+/**
+ * @class list_element
+ * @brief Element of a chained list that contains a double value and is linked with previous and next values of the list :
+ * - if previous element is null : the element is the list head
+ * - if next element is null : the element is the list tail
+ * 
+ */
 class list_element {
+    /**
+     * @brief element value 
+     * 
+     */
     double _value;
-    list_element *_prev;
-    list_element *_next;
 
+    /**
+     * @brief pointer to the previous list element
+     * 
+     */
+    list_element *_prev;
+
+    /**
+     * @brief pointer to the next list element
+     * 
+     */
+    list_element *_next;
+    
 public:
+    /**
+     * @brief Construct a new list element
+     * 
+     * @param v the double value
+     * @param p pointer to the previous element
+     * @param n pointer to the next element
+     */
     list_element(double v=0.0, list_element *p=nullptr, list_element *n=nullptr):
         _value(v), _prev(p), _next(n) {}
+    
+    /**
+     * @brief Destroy the list element
+     * 
+     */
     ~list_element() {}
 
+    /**
+     * @brief Get the next list element
+     * 
+     * @return list_element* 
+     */
     list_element *get_next() {return _next;}
+
+    /**
+     * @brief Set the next list element
+     * 
+     * @param n the new element of the list that will be insert after the current
+     * element
+     */
     void set_next(list_element *n) {_next = n;}
+
+    /**
+     * @brief Get the previous list element
+     * 
+     * @return list_element* 
+     */
     list_element *get_prev() {return _prev;}
+
+    /**
+     * @brief Set the previous list element
+     * 
+     * @param n the new element of the list that will be insert before the current
+     * element
+     */
     void set_prev(list_element *n) {_prev = n;}
+
+    /**
+     * @brief Get the element value
+     * 
+     * @return double 
+     */
     double get_value()const {return _value;}
+
+    /**
+     * @brief Set the element value
+     * 
+     * @param v the new double value
+     */
     void set_value(double v) {_value = v;}
 };
 
