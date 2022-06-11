bd3f882b333774ae1213d1bfabb3cb6adb806a89
ahmssy <57914110+ahmssy@users.noreply.github.com>
2022-06-11 14:19:06 +0200

    refarctorisation

--- manager.h
+++ manager.h
@@ -5,6 +5,20 @@
 
 class storage_type { // pure virtual
     /* Declarations */
+
+public :
+
+    //add element
+    virtual void push_back();
+
+    //delete one element
+    virtual void pop_element();
+
+    //get an iterator on the storage
+    virtual void iterator();
+
+    //TODO histoire d'opérateur poto
+
 };
 // list_storage, vector_storage inherit from storage_type
 
--- process.h
+++ process.h
@@ -1,11 +1,17 @@
 #ifndef PROCESS_H
 #define PROCESS_H
 
+#include "storage_type.h"
 
-class process
-{
+class process { // raw pointers may be improved
+    storage_type *_storage;
+    loader *_loader;
+    calculation *_calc;
+    writer *_writer;
+
 public:
-    process();
+    process(storage_type *stor, loader *load, calculation *calc, writer *write);
+    void execute();
 };
 
 #endif // PROCESS_H
